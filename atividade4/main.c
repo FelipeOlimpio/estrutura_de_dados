503c2bd25c849c8ae77da42a404a5eac436a7d74
FelipeOlimpio <137361340+FelipeOlimpio@users.noreply.github.com>
2023-09-11 11:05:23 -0300

    Add files via upload

---
+++ atividade4/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bubble_sort.h"
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s <lista de números>\n", argv[0]);
+        return 1;
+    }
+
+    int n = argc - 1;
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    for (int i = 1; i <= n; i++) {
+        arr[i - 1] = atoi(argv[i]);
+    }
+
+    bubbleSort(arr, n);
+
+    printf("Saída: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    free(arr);
+    return 0;
+}
